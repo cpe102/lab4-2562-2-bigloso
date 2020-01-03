be4e39169a9bb6f13dd3da85c7a4e1c580cf5c1d
Bigloso <wayside.big@gmail.com>
2020-01-03 16:17:06 +0700

    vv1

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+#include <cmath>
 
+using namespace std;
+ findDivisor(int x){
+     
+     int i =2 ;
+     while(i>0){
+         if(x%i == 0){
+             return i ;
+         }
+         else {
+             i++ ;
+         } 
+     }
+ }
+int main(){
+    int x ;
+    cout << "input =" ;
+    cin >> x ;
+    cout << "outout ="<< findDivisor(x) ;
+    return 0 ;
+
+}
