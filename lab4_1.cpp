aa26ca17387060fb75b212af9577ac75cb861e7b
Bigloso <wayside.big@gmail.com>
2020-01-03 15:38:51 +0700

    V11

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
-#include <string>
+#include <cmath>
 using namespace std;
 int main(){
-    float x , y ,z;
+    float x , y ,z ,A , B;
     float e = 2.71828 ;
     cout <<"Enter x =" ;
     cin>>x ;
     y=x*2+1 ;
-    while(y>1){
-        z=2*2 ;
-        y=y-1 ;
-    }
-    
-
+    A=3*pow(x,3)+2*e+pow(2,y)+sqrt(x*x+1) ;
+    cout<<"result y = "<<A ;
     
     
     return 0 ;
