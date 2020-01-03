6e86aa39dc213c4d5f7d942608c2cc8968950a3c
Bigloso <wayside.big@gmail.com>
2020-01-03 15:53:53 +0700

    v12

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
+#include <cmath>
 
 using namespace std;
 
-//Write function findDistance() here
+float findDistance(float u,float a,float t){
+float s ;
+s=u*t+0.5*a*pow(t,2);
+return s;
+ } 
 
 int main(){
+  cout << "s =" << findDistance(1.5,-1,2) ;
 
-  //Calling findDistance() using test cases
+  
 
   return 0;
 }
