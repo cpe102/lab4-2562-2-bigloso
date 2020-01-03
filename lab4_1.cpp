8041215ba6dcf71f5ecd4d5cd238401bf7ba176b
Bigloso <wayside.big@gmail.com>
2020-01-03 15:30:47 +0700

    v1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -2,9 +2,19 @@
 #include <string>
 using namespace std;
 int main(){
-    int x ;
-    cout <<"Enter x" ;
+    float x , y ,z;
+    float e = 2.71828 ;
+    cout <<"Enter x =" ;
     cin>>x ;
+    y=x*2+1 ;
+    while(y>1){
+        z=2*2 ;
+        y=y-1 ;
+    }
     
+
+    
+    
+    return 0 ;
 
 }
